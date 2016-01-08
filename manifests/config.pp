951b7b83bbcb1b5cc9a3b8d1be7a929493bfe50b
Kris Buytaert <sdog@inuits.eu>
2016-01-08 15:02:44 +0100

    Adding 2 services

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,4 +5,10 @@ class overpass_api::config {
   file { '/etc/systemd/system/overpass-api.service':
     content => template('overpass_api/overpass-api.service')
   }
+  file { '/etc/systemd/system/dispatcher-areas.service':
+    content => template('overpass_api/dispatcher-areas.service')
+  }
+  file { '/etc/systemd/system/osm3s-query.service':
+    content => template('overpass_api/osm3s-query.service')
+  }
 }
